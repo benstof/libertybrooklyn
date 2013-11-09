ed519a2c7c2fb9adc485ebd757bfb48bdeb7e88c
Barend <barend@barends-imac.local>
2013-11-09 12:14:36 -0500

    Add

---
+++ index.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">  
+<html xmlns="http://www.w3.org/1999/xhtml">  
+<head>  
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />  
+    <title>Creatif</title>  
+    <link href="style.css" rel="stylesheet" type="text/css" />  
+  
+</head>  
+  
+<body>  
+
+
+    <div id="main">  
+      
+
+        <div class="container">  
+          
+
+            <div id="header">  
+              
+                <img src="assets/logo.png" />  <br/>
+                BROOKLYN <br/>
+                <H2>THERE'S ALWAYS A STORY BENEATH THE SURFACE.</H2>
+              
+            </div>  
+              
+
+
+
+            <div id="countdown" style="height:150px;">  
+              
+                BE PART OF OURS IN...
+
+              
+            </div>  
+              
+
+
+            <div id="splash">  
+              
+                <img src="temp.jpg" style="float:left; width:500px; height:300px;"/>  
+                <div style="width:400px; float:left; text-align:left; padding:20px;"> 
+
+<h3>WE ARE LIBERTY CHURCH BROOKLYN </h3>
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+                 </div>
+              
+            </div>  
+
+
+            <div id="signup" style="height:250px;">  
+              
+                Signup  
+              
+            </div>  
+              
+
+              
+            <div id="community">  
+              
+                <img src="temp.jpg" style="float:left; width:250px; height:180px;"/>  
+                <div style="width:400px; float:left; text-align:left; padding:20px;"> 
+
+<h3>COMMUNITY </h3>
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+                 </div>
+              
+              
+            </div>  
+
+            <div id="family">  
+              
+                <img src="temp.jpg" style="float:left; width:250px; height:180px;"/>  
+<div style="width:400px; float:left; text-align:left; padding:20px;"> 
+
+<h3>FAMILY </h3>
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+                 </div>
+            </div>  
+              
+
+              
+            <div id="generations">  
+              
+                <img src="temp.jpg" style="float:left; width:250px; height:180px;"/>  
+<div style="width:400px; float:left; text-align:left; padding:20px;"> 
+
+<h3>GENERATIONS </h3>
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+Lorem ipsum dolor sit amet,  consectetur adipisicing elit, sed 
+                 </div>
+            </div>  
+
+
+          
+        </div>  
+      
+    </div>  
+
+
+
+  
+    <div id="footer">  
+  
+        <div class="container">  
+          
+            Footer Stuff Goes in Here  
+          
+        </div>  
+      
+
+
+
+    </div>  
+
+
+
+</body>  
+</html> 
