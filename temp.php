bac67d8d8604673756241731084a48353a6a1e74
Barend <barend@barends-imac.local>
2013-11-11 22:19:29 -0500

    Add copy

---
+++ temp.php
@@ -0,0 +1,251 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">  
+<html xmlns="http://www.w3.org/1999/xhtml">  
+<head>  
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />  
+    <title>Liberty Church Brooklyn</title>  
+    <link href="style.css" rel="stylesheet" type="text/css" />  
+  <script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+  <script src="cd.js"></script> 
+
+</head>  
+  
+<body>  
+
+
+    <div id="main">  
+      
+
+        <div class="container">  
+          
+
+            <div id="header" style="color:#fff;">  
+              
+                <img src="assets/logo.png" />  <br/>
+                BROOKLYN <br/>
+                <H2>THERE'S ALWAYS A STORY BENEATH THE SURFACE.</H2>
+              
+            </div>  
+              
+
+
+
+            <div id="countdown" style="height:150px;color:#fff; margin-bottom:20px;">  
+              
+                <div style="width:100%; margin-bottom:20px; float:left;" >BE PART OF OURS IN...</div>
+
+
+
+                <div style="width:100px; margin-left:210px; float:left;" >
+
+                    <div style="border-radius: 50%;width: 100px; color:#888;  position:relative; height: 100px;opacity:0.5; background: #fff;">
+                      <span id="days" style="float:left;font-size:22px;font-weight:bold;margin-top:30px;text-align:center;width:100%;"></span>
+                      <span style="float:left;font-size:11px;font-weight:bold;margin-top:0px;text-align:center;width:100%;">DAYS</span>
+                    
+                    </div>
+
+                </div>
+                <div style="width:100px; margin-left:50px;  float:left;" >
+
+                    <div style="border-radius: 50%;width: 100px; color:#888;  position:relative; height: 100px;opacity:0.5; background: #fff;">
+                      <span id="hours"  style="float:left;font-size:22px;font-weight:bold;margin-top:30px;text-align:center;width:100%;"></span>
+                      <span style="float:left;font-size:11px;font-weight:bold;margin-top:0px;text-align:center;width:100%;">HOURS</span>
+                    
+                    </div>
+
+                </div>
+                <div style="width:100px; margin-left:50px;  float:left;" >
+
+                    <div style="border-radius: 50%;width: 100px; color:#888; position:relative; height: 100px;opacity:0.5; background: #fff;">
+                      <span id="minutes"  style="float:left;font-size:22px;font-weight:bold;margin-top:30px;text-align:center;width:100%;"></span>
+                      <span style="float:left;font-size:11px;font-weight:bold;margin-top:0px;text-align:center;width:100%;">MINUTES</span>
+                    </div>
+
+                </div>
+                <div style="width:100px; margin-left:50px;   float:left;" >
+
+                    <div style="border-radius: 50%;width: 100px;  color:#888; position:relative; height: 100px;opacity:0.5; background: #fff;">
+                      <span id="seconds"  style="float:left;font-size:22px;font-weight:bold;margin-top:30px;text-align:center;width:100%;"></span>
+                      <span style="float:left;font-size:11px;font-weight:bold;margin-top:0px;text-align:center;width:100%;">SECONDS</span>
+                    
+                    </div>
+
+                </div>
+              
+            </div>  
+              
+
+
+            <div id="splash">  
+              
+                <img src="temp.jpg" style="float:left; width:500px; height:300px;"/>  
+                <div style="width:43%; float:left; text-align:left; padding:20px; font-size:14px;"> 
+
+<h3 style="margin-top:0px;">WE ARE LIBERTY CHURCH BROOKLYN </h3>
+<p>Lead Pastors Paul and Andi Andrew moved from Sydney, Australia, in 2010 to plant Liberty Church in New York City, 
+with a God-inspired dream to establish a Community, a Catalyst, and a Cause, influencing a city that influences the world. </p>
+In 2011, they began services in Tribeca and launched a second community in Union Square the same year. 
+Church communities in Zimbabwe and London have also sprung up because of Liberty Church’s vision to impact ten 
+communities in ten years. <p>Liberty Church Brooklyn has always been a prized part of the vision, and we are thrilled 
+to begin weekly services at United Artists Cinemas on January 19, 2014.</p>
+
+                 </div>
+              
+            </div>  
+
+
+
+
+<!-- Begin MailChimp Signup Form -->
+<link href="//cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+<style type="text/css">
+    #mc_embed_signup{background:#fff; clear:left; font:14px Helvetica,Arial,sans-serif; }
+    /* Add your own MailChimp form style overrides in your site stylesheet or in this style block.
+       We recommend moving this block and the preceding CSS link to the HEAD of your HTML file. */
+</style>
+
+
+
+<!--End mc_embed_signup-->
+
+
+
+
+<form action="http://Libertychurchnyc.us7.list-manage1.com/subscribe/post?u=09b4790780fa287fe1443681c&amp;id=8cc32b942b" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+
+
+            <div id="signup" style="min-height:120px; text-align:left;">  
+              
+              <div id="mc_embed_signup">
+               <div style="float:left; width:40%;">
+                LEARN MORE. <BR/> <span style="color: #ac2324">GET CONNECTED.</span><BR/><BR/>
+                <span style="font-size:12px;" >Sign up to learn more about Liberty Church Brooklyn  </span>
+               </div>
+
+
+               <div style="float:left; width:30%;">
+
+               <input style="border:1px solid #CCCCCC;padding:15px;width:85%;" type="text" name="FNAME" class="" id="mce-FNAME" 
+               value="Full Name"/>
+
+
+               <div style="width:100%; float:left; font-size:12px; padding-left:10px; padding-top:20px;">
+                <span style="width:225px; padding-top:10px; float: left;">I'd like to meet a team member for coffee. </span>
+                
+                <input type="checkbox" ame="group[17753][1]" id="mce-group[17753]-17753-0" checked=""/>
+
+
+                <label for="checkboxG5" class="css-label"></label>
+                <input class="css-checkbox" style="outline:1px solid #ccc; " type="checkbox" />
+  
+               </div>
+               </div>
+
+               <div style="float:left; width:30%;">
+
+                <input style="border:1px solid #CCCCCC;padding:15px;width:85%;"  type="email" value="Email" name="EMAIL" class="required email" id="mce-EMAIL" />
+
+               
+
+               <div style="width:96%; float:left; font-size:12px; padding-left:0px; padding-top:10px;">
+
+                <input style=" width:100%; height: 50px; border:0px solid #ccc; background-color: #ac2324; 
+                color:#fff; font-weight:bold;" 
+                type="submit"  value="SIGN UP" /></div>
+               </div>
+
+
+
+
+
+
+
+
+
+
+                </form>
+
+
+                <div id="mce-responses" class="clear">
+        <div class="response" id="mce-error-response" style="display:none"></div>
+        <div class="response" id="mce-success-response" style="display:none"></div>
+    </div>  
+
+
+
+
+</div>
+
+
+
+            </div>  
+              
+
+              
+            <div id="community">  
+              
+                <img src="temp.jpg" style="float:left; width:250px; height:180px;"/>  
+                <div style="width:68%; float:left; text-align:left; padding:20px; font-size:14px;"> 
+
+<h3 style="margin-top:0px;">COMMUNITY </h3>
+<p>We are a church that believes that you were created for connection, and that everyone matters to God. </p>
+“Growing up my community was a 3 block radius in Bed Stuy, and now after 
+connecting with Liberty Church, my community has grown beyond these borders and changed my life.” Vanessa
+                 </div>
+              
+              
+            </div>  
+
+            <div id="family">  
+              
+                <img src="temp.jpg" style="float:left; width:250px; height:180px;"/>  
+<div style="width:68%;  float:left; text-align:left; padding:20px; font-size:14px;"> 
+
+<h3 style="margin-top:0px;">FAMILY </h3>
+<p>We are a family of believers from all backgrounds and stages of life.</p>
+“These are MY people. My son was placed in my arms in a crowded Brooklyn hospital, elbow to elbow with other 
+families from other cultures celebrating the same miracle in unique ways. Everyone here is Brooklyn.” Emily
+
+                 </div>
+            </div>  
+              
+
+              
+            <div id="generations">  
+              
+                <img src="temp.jpg" style="float:left; width:250px; height:180px;"/>  
+<div style="width:68%;  float:left; text-align:left; padding:20px; font-size:14px;"> 
+
+<h3 style="margin-top:0px;">GENERATIONS </h3>
+<p>Our “2020 Vision” is to impact 10 communities in the next 10 years through life-giving local churches.
+“Brooklyn is strategic. </p>It always has been. When the Declaration of Independence was signed, 
+the first place they came for was Brooklyn. MY Brooklyn. Only big things start here. Only strong things grow here.” 
+
+                 </div>
+            </div>  
+
+
+          
+        </div>  
+      
+    </div>  
+
+
+
+  
+    <div id="footer">  
+  
+        <div class="container">  
+          
+            
+          
+        </div>  
+      
+
+
+
+    </div>  
+
+
+
+</body>  
+</html> 
